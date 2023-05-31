0e3e912d110665d686081a8884a8b09f653459d0
寺本諭史 <fashsato11@gmail.com>
2023-05-31 22:16:14 +0900

    カウンターを設置してカウントアップする

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,33 @@ import Head from "next/head";
 import type { NextPage } from "next";
 import MainTag from "src/components/MainTag/MainTag";
 import Header from "src/components/Header/Header";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const Home: NextPage = () => {
+  const [num, setNum] = useState(1);
+
+  // コンポーネント内に記述する場合はuseCallbackを使う
+  function handleClick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    setNum((num) => num + 1);
+    setNum((num) => num + 1);
+  }
+
+  useEffect(() => {
+    document.body.style.backgroundColor = "lightblue";
+    return () => {
+      document.body.style.backgroundColor = "";
+    };
+  }, []);
+
   return (
     <>
       <Head>
         <title>Create Next App</title>
       </Head>
       <Header />
+      <h1>{num.toString()}</h1>
+      <button onClick={handleClick}>ボタン</button>
       <MainTag title="index" />
     </>
   );
